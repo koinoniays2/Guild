7afcc14ecbe0584bc6bc10de5a0a5919983721eb
koinoniays2 <koinonia_ys@naver.com>
2024-02-01 01:18:09 +0900

    수정

--- src/components/MemberSearch.jsx
+++ src/components/MemberSearch.jsx
@@ -35,17 +35,20 @@ export default function MemberSearch({searchName, guildMember}) {
     // console.log(guildMemberCharacterStat);
 
     function formatStatValue(value) {
-        return value?.stat_value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        return value?.stat_value ? value?.stat_value.replace(/\B(?=(\d{3})+(?!\d))/g, ',') : "-";
     }
+    const FLEX = `w-full flex justify-center items-center`;
+    const FLEX_COL = `w-full flex flex-col justify-center items-center`;
+    const STAT_NAME = `font-bold text-gray-400 text-sm px-2`;
 
     return (
         <section className="relative w-full flex flex-col justify-center items-center text-black-color py-14">
             <div className="w-[90%] max-w-5xl flex flex-col items-center space-y-2">
                 {   
                     guildMember?.includes(searchName) ?
-                    // 캐릭터 네임, 이미지, 직업, 레벨
                     <>
-                        <div className="w-full flex flex-col items-center">
+                        {/* 캐릭터 네임, 이미지, 직업, 레벨 */}
+                        <div className={FLEX_COL}>
                             <p className="text-lg font-bold">{dataGuildMemberCharacter?.character_name}</p>
                             <img src={dataGuildMemberCharacter?.character_image} alt="character img" />
                             <div className="flex justify-center items-end space-x-2">
@@ -54,47 +57,44 @@ export default function MemberSearch({searchName, guildMember}) {
                             </div>
                         </div>
                         {/* 스텟 정보 */}
-                        <div className="w-full max-w-[360px] flex flex-col justify-center items-center border border-[#5CB85C]">
+                        <div className={`${FLEX_COL} max-w-[360px] border border-[#5CB85C]`}>
                             {/* 스공 */}
                             {guildMemberCharacterStat && (
-                            <div className="w-full flex justify-center items-center border-b">
+                            <div className={`${FLEX} border-b`}>
                                 <p className="font-bold text-gray-400 text-sm px-2">스공</p>
                                 <p>{`${formatStatValue(guildMemberCharacterStat[0])} 
                                 ~ ${formatStatValue(guildMemberCharacterStat[1])}`}</p>
                             </div>
                             )}
-                            {/* 스텟 */}
+                            {/* 능력치 */}
                             {guildMemberCharacterStat && (
-                            <div className="flex flex-col justify-center items-center">
-                                <div className="flex justify-center items-center">
+                            <div className={FLEX_COL}>
+                                <div className={FLEX}>
                                     {/* HP */}
-                                    <p className="font-bold text-gray-400 text-sm px-2">{guildMemberCharacterStat[20]?.stat_name}</p>
+                                    <p className={STAT_NAME}>{guildMemberCharacterStat[20]?.stat_name}</p>
                                     <p>{formatStatValue(guildMemberCharacterStat[20])}</p>
                                     {/* STR */}
-                                    <p className="font-bold text-gray-400 text-sm px-2">{guildMemberCharacterStat[16]?.stat_name}</p>
+                                    <p className={STAT_NAME}>{guildMemberCharacterStat[16]?.stat_name}</p>
                                     <p>{formatStatValue(guildMemberCharacterStat[16])}</p>
                                     {/* DEX */}
-                                    <p className="font-bold text-gray-400 text-sm px-2">{guildMemberCharacterStat[17]?.stat_name}</p>
+                                    <p className={STAT_NAME}>{guildMemberCharacterStat[17]?.stat_name}</p>
                                     <p>{formatStatValue(guildMemberCharacterStat[17])}</p>
                                 </div>
-                                <div className="flex justify-center items-center border-b">
-                                    {/* HP */}
-                                    <p className="font-bold text-gray-400 text-sm px-2">{guildMemberCharacterStat[21]?.stat_name}</p>
+                                <div className={FLEX}>
+                                    {/* MP */}
+                                    <p className={STAT_NAME}>{guildMemberCharacterStat[21]?.stat_name}</p>
                                     <p>{formatStatValue(guildMemberCharacterStat[21])}</p>
-                                    {/* STR */}
-                                    <p className="font-bold text-gray-400 text-sm px-2">{guildMemberCharacterStat[18]?.stat_name}</p>
+                                    {/* INT */}
+                                    <p className={STAT_NAME}>{guildMemberCharacterStat[18]?.stat_name}</p>
                                     <p>{formatStatValue(guildMemberCharacterStat[18])}</p>
-                                    {/* DEX */}
-                                    <p className="font-bold text-gray-400 text-sm px-2">{guildMemberCharacterStat[19]?.stat_name}</p>
+                                    {/* LUK */}
+                                    <p className={STAT_NAME}>{guildMemberCharacterStat[19]?.stat_name}</p>
                                     <p>{formatStatValue(guildMemberCharacterStat[19])}</p>
                                 </div>
                             </div>
                             )}
                         </div>
                     </>
-
-
-
                     :
                     searchName === "" ? "" : <p>길드원이 아닙니다.</p>
                 }
