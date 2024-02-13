cfc741ce755f9b94d2755ab2bf571a5ebc30d405
koinoniays2 <koinonia_ys@naver.com>
2024-02-13 12:56:32 +0900

    펫수정

--- src/components/PetEquipment.jsx
+++ src/components/PetEquipment.jsx
@@ -25,17 +25,17 @@ export default function PetEquipment({pet}) {
         pet && (
         <section className="w-full max-w-80 flex flex-col items-center justify-center space-y-5 p-2 rounded-lg bg-gray-200">
             <p className="w-full text-black-color uppercase font-bold text-left">pet</p>
-            <div className="w-full flex flex-col justify-between items-center space-y-2">
-                <div className="flex space-x-1">
-                    <EquipmentSlot petIcon={pet.pet_1_icon || ""} pet={pet1} />
-                    <EquipmentSlot petIcon={pet.pet_1_equipment?.item_icon || ""} pet={pet1.pet_1_equipment}/>
-                </div>
-                <div className="flex space-x-1">
+            <div className="w-full flex items-center justify-center space-x-2.5">
+                <div className="flex flex-col space-y-1">
+                    <p className="text-center text-gray-600">펫</p>
                     <EquipmentSlot petIcon={pet.pet_2_icon || ""} pet={pet2}/>
-                    <EquipmentSlot petIcon={pet.pet_2_equipment?.item_icon || ""} pet={pet2.pet_2_equipment}/>
-                </div>
-                <div className="flex space-x-1">
                     <EquipmentSlot petIcon={pet.pet_3_icon || ""} pet={pet3}/>
+                    <EquipmentSlot petIcon={pet.pet_1_icon || ""} pet={pet1} />
+                </div>
+                <div className="flex flex-col space-y-1">
+                    <p className="text-center text-gray-600">장비</p>
+                    <EquipmentSlot petIcon={pet.pet_1_equipment?.item_icon || ""} pet={pet1.pet_1_equipment}/>
+                    <EquipmentSlot petIcon={pet.pet_2_equipment?.item_icon || ""} pet={pet2.pet_2_equipment}/>
                     <EquipmentSlot petIcon={pet.pet_3_equipment?.item_icon || ""} pet={pet3.pet_3_equipment}/>
                 </div>
             </div>
