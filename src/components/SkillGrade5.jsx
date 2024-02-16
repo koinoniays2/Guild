38319a309a60dfa4834a252491402e8eb25ffd70
koinoniays2 <koinonia_ys@naver.com>
2024-02-16 16:42:05 +0900

    스킬수정

--- src/components/SkillGrade5.jsx
+++ src/components/SkillGrade5.jsx
@@ -1,12 +1,12 @@
 import SkillSlot from "./SkillSlot";
 
-const ThreeRow = ({children}) => (
-    <div className="flex flex-wrap w-full justify-center items-center space-x-3.5">
+const FourRow = ({children}) => (
+    <div className="flex w-full flex-wrap justify-center items-center space-x-3">
         {children}
     </div>
 )
-const FourRow = ({children}) => (
-    <div className="flex w-full justify-center items-center space-x-4">
+const ThreeRow = ({children}) => (
+    <div className="flex flex-wrap w-full justify-center items-center space-x-4">
         {children}
     </div>
 )
@@ -15,12 +15,31 @@ export default function Skill({skill}) {
   return (
     <section className="bg-[#1f354d80] w-full max-w-80 p-3 space-y-2 rounded-lg">
         <p className="text-white-color font-bold">V 매트릭스</p>
-        <div className="flex flex-wrap w-full justify-center items-center space-x-4">
+        <FourRow>
             <SkillSlot />
             <SkillSlot />
             <SkillSlot />
             <SkillSlot />
-        </div>
+        </FourRow>
+        <ThreeRow>
+            <SkillSlot />
+            <SkillSlot />
+            <SkillSlot />
+        </ThreeRow>
+        <FourRow>
+            <SkillSlot />
+            <SkillSlot />
+            <SkillSlot />
+            <SkillSlot />
+        </FourRow>
     </section>
   )
 }
+
+{/* <div className="flex flex-wrap w-full justify-center items-center">
+    {skill?.map((item, index) => (
+        (index) % 7 === 4 ?
+            <SkillSlot key={index} style={marginLeft} skill={item} /> :
+            <SkillSlot key={index} skill={item} />
+        ))}
+</div> */}
