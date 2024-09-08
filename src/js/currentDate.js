28d23f66d745cd606d4de00648a7a90af1f2087c
koinoniays2 <koinonia_ys@naver.com>
2024-09-09 01:57:31 +0900

    [update] 날짜 기준 수정(-27시간)

--- src/js/currentDate.js
+++ src/js/currentDate.js
@@ -1,7 +1,7 @@
 export default function getCurrentDate() {
     const today = new Date();
     // today.setDate(today.getDate() - 1);
-    today.setHours(today.getHours() - 25);
+    today.setHours(today.getHours() - 27);
 
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0');
